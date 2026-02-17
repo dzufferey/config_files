69eb4cc275ebaf13693d864dad34802b3143e6dd
Damien Zufferey <rilaak@gmail.com>
2026-02-17 22:14:34 +0100

    switch from ollama to Jan

--- nvim/lua/plugins/avante.lua
+++ nvim/lua/plugins/avante.lua
@@ -11,12 +11,13 @@ return {
     ---@type avante.Config
     opts = {
       -- add any opts here
-      provider = "ollama",
+      provider = "jan",
       providers = {
-        ollama = {
-          endpoint = "http://localhost:11434",
-          model = "devstral:latest",
-          disable_tools = true, -- TODO: check if devstral supports tools
+        jan = {
+          __inherited_from = "openai",
+          api_key_name = "JAN_API_KEY",
+          endpoint = "http://127.0.0.1:11434/v1",
+          model = "Devstral-Small-2-24B-Instruct-2512-Q8_0",
         },
       },
     },
